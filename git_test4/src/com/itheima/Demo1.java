25a0fd05ed947c746cf19546350a83af154a83bc
aaa <aaa@itcast.com>
2018-10-16 13:07:28 +0800

    czvxcv

---
+++ git_test4/src/com/itheima/Demo1.java
@@ -0,0 +1,19 @@
+package com.itheima;
+
+public class Demo1 {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+
+    public void test(){
+        System.out.println("fasdfa");
+        System.out.println("fsdafas");
+    }
+}
